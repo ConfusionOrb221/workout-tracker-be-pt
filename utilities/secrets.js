a7f25fad0281ed308713704fe7ba02bf8c6c5a4e
Nathan <nate1898@gmail.com>
2021-02-11 21:07:21 -0500

    Try this

--- utilities/secrets.js
+++ utilities/secrets.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    jwtSecret: JSON.parse(`"${process.env.JWT_SECRET}"`) || "Peanut_Butter_Jelly_Time",
-    jwtPublic: JSON.parse(`"${process.env.JWT_PUBLIC}"`) || "Test"
+    jwtSecret: `${process.env.JWT_SECRET}` || "Peanut_Butter_Jelly_Time",
+    jwtPublic: `${process.env.JWT_PUBLIC}` || "Test"
 };
